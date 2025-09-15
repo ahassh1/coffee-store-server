ad262a18ad06acba314ac4351186d1a9c827b009
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-09-15 11:34:54 +0600

    server setup + mongodb connect

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-app.get("/", (req, res) => {
-  res.send("HelloUp!");
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cros = require("cors");
+const app = express("cros");
+const port = process.env.PORT || 3000;
+app.use(cros());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ahassh.9fhai46.mongodb.net/?retryWrites=true&w=majority&appName=ahassh`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("coffee crud");
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`coffee server is getting on port`);
 });
